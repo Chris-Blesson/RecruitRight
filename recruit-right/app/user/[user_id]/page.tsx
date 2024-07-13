a330964774baccb6a18e229bcd942f2eee14ef53
Lokprakash-babu <lokprakash100@gmail.com>
2024-07-13 13:51:11 +0530

    skills

--- recruit-right/app/user/[user_id]/page.tsx
+++ recruit-right/app/user/[user_id]/page.tsx
@@ -1,11 +1,12 @@
 import Education from "./JobSeekerDetails/Education";
 import Projects from "./JobSeekerDetails/Projects";
+import Skills from "./JobSeekerDetails/Skills";
 import Work from "./JobSeekerDetails/Work";
 
 const UserDetails = () => {
   return (
     <div>
-      <Projects />
+      <Skills />
     </div>
   );
 };
