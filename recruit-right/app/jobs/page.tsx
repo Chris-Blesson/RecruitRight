c82e660bbb28eae4ca59ac85dda106848bd5da45
Lokprakash Babu <devlokprakash100@gmail.com>
2024-07-14 23:31:24 +0530

    job listing page (#16)

--- recruit-right/app/jobs/page.tsx
+++ recruit-right/app/jobs/page.tsx
@@ -1,7 +1,19 @@
-import ResumeParserProgress from "../components/ResumeParserProgress";
+import { fetchJobListing } from "@/lib/fetchJobListing";
+import { notFound } from "next/navigation";
+import JobListingTable from "./JobListingTable";
 
-const Jobs = () => {
-  return <ResumeParserProgress />;
+const Jobs = async () => {
+  try {
+    const jobs = await fetchJobListing();
+    if (!jobs || !jobs.length) {
+      return <p>No jobs found</p>;
+    }
+    console.log("jobs", jobs);
+    return <JobListingTable jobListing={jobs} />;
+  } catch (err) {
+    console.log("jobs error", err);
+    return notFound();
+  }
 };
 
 export default Jobs;
---
+++ recruit-right/constants/jobStatus.ts
@@ -0,0 +1,5 @@
+export enum JOB_STATUS {
+  HIRING = "hiring",
+  YET_TO_HIRE = "yet-to-hire",
+  COMPLETED = "completed",
+}
