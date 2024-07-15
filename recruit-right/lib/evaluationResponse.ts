971e38d58c92520dc4b7c6c18babb96f1403884e
Lokprakash Babu <devlokprakash100@gmail.com>
2024-07-15 14:03:42 +0530

    test evaluation ai integration (#19)

--- recruit-right/lib/evaluationResponse.ts
+++ recruit-right/lib/evaluationResponse.ts
@@ -1,10 +1,12 @@
 import { SUBMISSION_STATUS } from "@/constants/submissionStatus";
 import { knex } from "./db";
+import { responseEvaluator } from "./openAi/responseEvaluator";
 
 export const evaluationResponse = async ({
   accountId,
   testResponse,
   jobId,
+  role,
   processId,
 }) => {
   const trxProvider = knex.transactionProvider();
@@ -12,12 +14,40 @@ export const evaluationResponse = async ({
   try {
     //TODO:Evaluate the response
 
-    console.log("evaluate response");
+    const modifiedTestResponse = Object.keys(testResponse).map((question) => {
+      return {
+        question,
+        answer: testResponse[question],
+      };
+    });
+
+    const accountDetailsResume = await knex("accounts")
+      .where({
+        account_id: accountId,
+      })
+      .select(["resume_payload"])
+      .first();
+    console.log("account details", accountDetailsResume);
+
+    const evaluateResponse = JSON.stringify({
+      relevancy_score: 85,
+      reason:
+        "The candidate has a B.Tech in Information Technology and relevant work experience as a Software Engineer at Freshworks. They have demonstrated their technical skills through various projects and contributions to open source. However, the candidate's answers to the questions provided lack depth and specificity, which could indicate potential weaknesses in communication or problem-solving skills.",
+    });
+
+    //TODO: Remove mocking before prod
+    // const evaluateResponse = await responseEvaluator({
+    //   cvText: accountDetailsResume?.resume_payload,
+    //   testRespone: modifiedTestResponse,
+    //   role,
+    // });
+    console.log("evaluate response", evaluateResponse);
     //Update the evaluation in the submission table
     const updatePayload = {
       status: SUBMISSION_STATUS.COMPLETED,
       evaluation: JSON.stringify({
-        feedback: "Overeall good",
+        //@ts-ignore
+        feedback: JSON.parse(evaluateResponse),
       }),
     };
     await knex("submissions")
