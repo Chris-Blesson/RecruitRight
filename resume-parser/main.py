a29db69c4e9d267f451e8dcb0ea7c19449a6daec
Lokprakash-babu <lokprakash100@gmail.com>
2024-07-21 15:56:33 +0530

    async flow

--- resume-parser/main.py
+++ resume-parser/main.py
@@ -1,20 +1,31 @@
-from fastapi import FastAPI, File, Form, UploadFile
-from docUtils import extractTextFromPdf
+from fastapi import FastAPI
+import httpx
+from pydantic import BaseModel
+from docUtils import extractTextFromPdfUrl
 from resumeGenerator import generateJsonResume
 
 app = FastAPI()
 
-@app.post("/parse")
-async def parseResume( account_id: int = Form(...), file: UploadFile=File(...)):
 
-  extractedResumeText = await extractTextFromPdf(file)
+class ParseResumeRequest(BaseModel):
+    account_id: str
+    pdf_url: str
+    
+@app.post("/parse")
+async def parseResume(request: ParseResumeRequest):
+
+  #TODO: Change this url to prod
+  url = "http://127.0.0.1:3000/api/resume/parse-success"  # Example third-party API URL
+  extractedResumeText = await extractTextFromPdfUrl(request.pdf_url)
   openAiModel="gpt-3.5-turbo"
   #TODO: Use api key from env variable
   openAiKey=""
   jsonResume = generateJsonResume(extractedResumeText, openAiKey, openAiModel)
 
   print(extractedResumeText)
-  
-  return {"resume_payload": jsonResume, "account_id":account_id};
+  async with httpx.AsyncClient() as client:
+      response = await client.post(url, json={"resumePayload": jsonResume, "account_id":request.account_id})
+      response.raise_for_status()
+  return {"resume_payload": jsonResume, "account_id":request.account_id};
 
 # python3 -m uvicorn main:app --reload
